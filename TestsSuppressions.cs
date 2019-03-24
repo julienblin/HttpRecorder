e96ee009f5b94992e4b9cf8009ac91af1f3c27ad
Julien Blin <julien.blin@gmail.com>
2019-03-24 12:49:55 -0400

    Added test project.

---
+++ TestsSuppressions.cs
@@ -0,0 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1652:Enable XML documentation output", Justification = "Not needed for tests projects")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1615:Element return value must be documented", Justification = "Not needed for tests projects")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1611:Element parameters must be documented", Justification = "Not needed for tests projects")]
